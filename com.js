beedc7353503d4f38304c83fa7844f466ed7c02d
arun sharma <arun123gulshan@gmail.com>
2023-07-16 00:12:30 +0530

    hello

---
+++ com.js
@@ -0,0 +1,17 @@
+import axios  from "axios";
+import react ,{component , useEffect}from 'react'
+class arun extends component{
+    render(){
+        return(
+            <div>
+               
+               componentDidMount(){
+
+               }
+                    
+                
+            </div>
+        );
+    }
+
+}
